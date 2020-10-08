bce49e1d92465757fb595b71e1bbf344ccbad645
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-10-08 14:24:00 -0500

    created date model- belongsto adventure/ hasmany date

---
+++ models/date.js
@@ -0,0 +1,30 @@
+//Date (Table to hold dates)
+//only has a date field cannot be null
+// Date belongs to Aadventure
+// A Date can't be created without Adventure due to the foreign key constraint
+
+
+
+module.exports = function(sequelize, DataTypes) {
+    var Date = sequelize.define("Date", {
+      date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+    });
+  
+    
+    Date.associate = function(models) {
+        Date.belongsTo(models.Adventure, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+
+        Date.hasMany(models.Time, {
+            onDelete: "cascade"
+        });
+    };
+  
+    return Date;
+  };
