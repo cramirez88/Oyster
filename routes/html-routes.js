de71b80175bd06322f159cc3d655f5f2b888541b
Amor Andal <aandal77@gmail.com>
2020-10-15 05:36:30 -0500

    ALL ROUTES NOW WORKING

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,12 +5,19 @@ const db = require("../models");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
-
+//**ALL ROUTES WORKING, DO NOT ADJUST ANYTHING BELOW**
 module.exports = function(app) {
-  app.get("/", (req, res) => {
+  app.get("intro", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/login");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+  app.get("/signup", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/signup");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
@@ -26,6 +33,10 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
 
+  app.get("/", (req, res)=>{
+    res.render("intro");
+  })
+  
   app.get("/map", (req, res)=>{
     res.render("map");
   });
